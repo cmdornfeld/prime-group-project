f8ce0f6a5b3d600e0c69b4281d758f77db07ac43
Jamie Richison <jamierichi@yahoo.com>
2020-01-31 10:48:29 -0600

    created AdminGolfer

---
+++ src/components/AdminGolfersPage/AdminGolfersPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import AdminNav from '../AdminNav/AdminNav';
+
+export class AdminGolfersPage extends Component {
+    render() {
+        return (
+            <div>
+                <AdminNav />
+                <p>Admin Golfers</p>
+            </div>
+        )
+    }
+}
+
+export default AdminGolfersPage;
