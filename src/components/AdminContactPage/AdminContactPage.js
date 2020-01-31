75f4ab7484ec0e6d9431a35e0bc996c2b844d38f
Jamie Richison <jamierichi@yahoo.com>
2020-01-31 10:48:16 -0600

    created AdminContact

---
+++ src/components/AdminContactPage/AdminContactPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import AdminNav from '../AdminNav/AdminNav'
+
+class AdminContactPage extends Component {
+    render() {
+        return (
+            <div>
+                <AdminNav />
+                <p>Admin Contact</p>
+            </div>
+        )
+    }
+}
+
+export default AdminContactPage;
