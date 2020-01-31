e9c53af0d7d68f07db7662a2da72f325edb242aa
Elmi Elmi <elmielmi522@gmail.com>
2020-01-31 16:49:13 -0600

    added event route in public.router

--- server/routes/public.router.js
+++ server/routes/public.router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
-//Get all customer
+//Get all Videos
 router.get('/videos', (req, res)=>{
     console.log('video route for get');
     const queryVideos = 'SELECT "id", "url" FROM "videos"';
@@ -13,6 +13,15 @@ router.get('/videos', (req, res)=>{
         console.log('Error from SELECT videos quert ', error);
         res.sendStatus(500);
     })
+});
+
+router.get('/event-info', (req, res) => {
+    pool.query(`SELECT * FROM "event";`)
+        .then(results => res.send(results.rows[0]))
+        .catch(error => {
+            console.log('Error GETTING comments:', error);
+            res.sendStatus(500);
+    });
 });
 
 module.exports = router;
