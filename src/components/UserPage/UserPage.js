f5f6d0aa636b8e0747b467846fcf559c96852eee
Elmi Elmi <elmielmi522@gmail.com>
2020-01-31 15:16:03 -0600

    update user page

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -1,24 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import Nav from '../Nav/Nav'
 
-// this could also be written with destructuring parameters as:
-// const UserPage = ({ user }) => (
-// and then instead of `props.user.username` you could use `user.username`
-const UserPage = (props) => (
-  <div>
-    <Nav />
-    <p>home</p>
-  </div>
-);
+class UserPage extends Component {
 
-// Instead of taking everything from state, we just want the user info.
-// if you wanted you could write this code like this:
-// const mapStateToProps = ({user}) => ({ user });
-const mapStateToProps = state => ({
-  user: state.user,
-});
+  render() {
+    return (
+      <div>
+        <Nav />
+        <p>home</p>
+      </div>
+    );
+  }
+}
+const putReduxStateonProps = (reduxState) => ({
+  reduxState
+})
 
-// this allows us to use <App /> in index.js
-export default connect(mapStateToProps)(UserPage);
+export default connect(putReduxStateonProps)(UserPage);
