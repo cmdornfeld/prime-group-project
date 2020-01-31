f740afa577fc610af41b022809cbd0158affc8bc
Jamie Richison <jamierichi@yahoo.com>
2020-01-31 10:48:02 -0600

    created AdminAbout

---
+++ src/components/AdminAboutPage/AdminAboutPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import AdminNav from '../AdminNav/AdminNav';
+
+class AdminAboutPage extends Component {
+    render() {
+        return (
+            <div>
+                <AdminNav />
+                <p>Admin About</p>
+            </div>
+        )
+    }
+}
+
+export default AdminAboutPage
