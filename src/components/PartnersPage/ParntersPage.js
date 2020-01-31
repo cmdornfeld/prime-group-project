dcef029a18c1f9e22fd18cc90fef85f82e71f9d0
Jamie Richison <jamierichi@yahoo.com>
2020-01-31 09:55:17 -0600

    created PartnersPage

---
+++ src/components/PartnersPage/ParntersPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ParntersPage extends Component {
+    render() {
+        return (
+            <div>
+                <p>partners</p>
+            </div>
+        )
+    }
+}
+
+export default ParntersPage;
