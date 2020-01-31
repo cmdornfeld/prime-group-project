a3d84b148d69e197eab8ca8d35656196722a18a2
Elmi Elmi <elmielmi522@gmail.com>
2020-01-31 16:19:09 -0600

    display videos in public page

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -5,11 +5,25 @@ import Nav from '../Nav/Nav'
 
 class UserPage extends Component {
 
+  componentDidMount() {
+    this.getVideo();
+}
+
+getVideo = ()=> {
+  this.props.dispatch({ type: "GET_VIDEOS"});
+}
+
   render() {
     return (
       <div>
         <Nav />
         <p>home</p>
+        {JSON.stringify(this.props.reduxState.videoReducer)}
+        {this.props.reduxState.videoReducer.map( (item) => {
+          return(
+              <iframe src={item.url} width='auto' height='auto' />
+          )
+        })}
       </div>
     );
   }
