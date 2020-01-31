f1e7e65bdd010776013016ae67c7b3e591e990d6
Jamie Richison <jamierichi@yahoo.com>
2020-01-31 09:47:40 -0600

    created GolfersPage

---
+++ src/components/GolfersPage/GolfersPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class GolfersPage extends Component {
+    render() {
+        return (
+            <div>
+                <p>Golfers Page</p>
+            </div>
+        )
+    }
+}
+
+export default GolfersPage;
