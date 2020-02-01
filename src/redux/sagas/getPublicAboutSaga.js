04b18a81de0f4cc9c4af8cde05ce51f6d24cbb3d
Elmi Elmi <elmielmi522@gmail.com>
2020-02-01 15:54:47 -0600

    add get foundation saga

--- src/redux/sagas/getPublicAboutSaga.js
+++ src/redux/sagas/getPublicAboutSaga.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { put, takeLatest } from 'redux-saga/effects';
 
+//get mission
 function* getMission() {
     try{
         const getResponse = yield axios.get(`/api/public/mission`);
@@ -12,8 +13,21 @@ function* getMission() {
     }
 }
 
+//get foundation
+function* getFoundation() {
+    try{
+        const getResponse = yield axios.get(`/api/public/foundation`);
+        console.log(getResponse.data)
+        yield put({type: 'SET_FOUNDATION', payload: getResponse.data})
+    }
+    catch (error){
+        console.log(error); 
+    }
+}
+
 function* getMissionSaga() {
     yield takeLatest('GET_MISSION', getMission)
+    yield takeLatest('GET_FOUNDATION', getFoundation)
 }
 
 export default getMissionSaga;
