97f9a0e911b60f9a0bbcdc45f7f652e9d1e3dfcc
Elmi Elmi <elmielmi522@gmail.com>
2020-02-01 15:56:49 -0600

    dispaly mission and foundation public aboutt page

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -1,16 +1,45 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Nav from '../Nav/Nav'
 
 class AboutPage extends Component {
+
+  componentDidMount() {
+    this.getMission();
+    this.getFoundation();
+}
+  
+  getMission = ()=> {
+    this.props.dispatch({ type: "GET_MISSION"});
+  }
+
+  getFoundation = ()=> {
+    this.props.dispatch({ type: "GET_FOUNDATION"});
+  }
+
   render() {
     return (
       <div>
         <Nav />
-        <p>about</p>
+        <h1>about</h1>
+        <p style={{color:'white'}}>{this.props.missionReducer.about}</p>
+        {this.props.foundationReducer.map( (item) => {
+          return(
+            <div>
+              <h3>{item.name}</h3>,
+              <img src={item.url} alt={item.name} width='220px' height='200px' />,
+              <p>{item.bio}</p>
+              </div>
+          )
+        })}
       </div>
     )
   }
 }
+const putReduxStateOnProps = (reduxStore) => ({
+  missionReducer: reduxStore.missionReducer,
+  foundationReducer: reduxStore.foundationReducer
+});
 
-export default AboutPage;
+export default connect(putReduxStateOnProps) (AboutPage);
