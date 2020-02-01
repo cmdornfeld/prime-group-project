336bda17807d234bd003d871c22990906e81db29
Elmi Elmi <elmielmi522@gmail.com>
2020-02-01 15:03:46 -0600

    add public about mission reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -6,6 +6,7 @@ import eventInfoReducer from './eventInfoReducer';
 import videoReducer from './publicVideos';
 import contactInfoReducer from './contactInfoReducer';
 import addressInfoReducer from './addressInfoReducer';
+import missionReducer from './getMissionReducer';
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -21,6 +22,7 @@ const rootReducer = combineReducers({
   videoReducer,
   contactInfoReducer,
   addressInfoReducer,
+  missionReducer,
 });
 
 export default rootReducer;
