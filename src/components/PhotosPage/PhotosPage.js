f4c996f0b3fbc039030d890a43af8da2a9bc73c6
Elmi Elmi <elmielmi522@gmail.com>
2020-02-08 15:14:49 -0600

    remove photPage.css

--- src/components/PhotosPage/PhotosPage.js
+++ src/components/PhotosPage/PhotosPage.js
@@ -4,8 +4,6 @@ import {withStyles} from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 
-import './photosPage.css'
-
 import Nav from '../Nav/Nav';
 
 const styles = {
