364dac0ba644c44187b63d600fc582b2646cc13a
Elmi Elmi <elmielmi522@gmail.com>
2020-02-08 15:12:49 -0600

    added maretial ui style

--- src/components/PhotosPage/PhotosPage.js
+++ src/components/PhotosPage/PhotosPage.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import {withStyles} from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+
+import './photosPage.css'
 
 import Nav from '../Nav/Nav';
+
+const styles = {
+    card: {
+        width: 'auto',
+        height: 'auto',
+        padding: 90,
+        margin: 100,
+        textAlign: 'center'
+    }
+};
 
 class PhotosPage extends Component {
 
@@ -14,17 +29,24 @@ class PhotosPage extends Component {
     }
 
     render() {
+        const {classes} = this.props;
         return (
             <div>
                 <Nav />
-                <p>photos</p>
-                {this.props.photosReducer.map( (item) => {
-                return(
-                    <div>
-                        <img src={item.url} alt={item.description} width='220px' height='200px' />
-                    </div>
-                    )
-                    })}
+                <h1 style={{textAlign:'center', fontSize:'50px', fontFamily:'serif'}}>100 HOLES FOR HOPE PHOTOS</h1>
+                        <Card className={classes.card}>
+                            <Grid container spacing={3} justify='center'>
+                            {this.props.photosReducer.map( (item) => {
+                            return(
+                                <Grid item>
+                                <div style={{ textAlign:'center'}}>
+                                    <img src={item.url} alt={item.description} width='220px' height='330px'  width='240px' height='200px' textAlign='center'/>
+                                </div>
+                                </Grid>
+                                )
+                                })}
+                            </Grid>
+                        </Card>
             </div>
         )
     }
@@ -33,4 +55,4 @@ const putReduxStateOnProps = (reduxStore) => ({
     photosReducer: reduxStore.photosReducer
 });
 
-export default connect(putReduxStateOnProps)(PhotosPage);
+export default connect(putReduxStateOnProps)(withStyles(styles)(PhotosPage));
