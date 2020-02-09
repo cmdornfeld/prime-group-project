4971d809390d96a52556968605a7132fcfa3093b
Chad Dornfeld <cmdornfeld@gmail.com>
2020-02-09 15:00:50 -0600

    updated styling for ContactPage.js

--- src/components/ContactPage/ContactPage.js
+++ src/components/ContactPage/ContactPage.js
@@ -1,8 +1,32 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
 import Nav from '../Nav/Nav';
-import './contactPage.css'
+import './contactPage.css';
+
+const styles = theme => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing.unit,
+      textAlign: 'center',
+      color: '#253055',
+      margin: theme.spacing.unit * 2,
+      backgroundColor: '#A3BED9'
+    },
+    textField: {
+        marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
+      },
+});
 
 class ContactPage extends Component {
 
@@ -42,84 +66,103 @@ class ContactPage extends Component {
         })
     }
 
-    render() {
+    render(props) {
+
+        const { classes } = this.props;
+
         return (
             <Fragment>
-            <div>
                 <Nav />
                 <h1>Contact</h1>
-                <div class='address'>
-                    {this.props.addressPublicReducer.map( (item) => {
-                        return(
-                            <div key= {item.id}>
-                                <h3>Address</h3>
-                                <p>{item.street}</p>
-                                <p>{item.city}, {item.state} {item.zip}</p>
+                <div className={classes.root} style={{width: '65%', margin: '0 auto'}}>
+                    <Grid container spacing={4}>
+                        <Grid item xs>
+                            <form>
+                                <Paper className={classes.paper}>
+                                    <TextField
+                                        required
+                                        id="standard-required"
+                                        label="Email"
+                                        placeholder="Enter your email"
+                                        value={this.state.email}
+                                        onChange={this.handleInputChangeFor('email')}
+                                        className={classes.textField}
+                                        margin="normal"
+                                        variant="outlined"
+                                        style={{width: '30%'}}
+                                    />
+                                    <TextField
+                                        required
+                                        id="standard-required"
+                                        label="Name"
+                                        placeholder="Enter your name"
+                                        value={this.state.name}
+                                        onChange={this.handleInputChangeFor('name')}
+                                        className={classes.textField}
+                                        margin="normal"
+                                        variant="outlined"
+                                        style={{width: '30%'}}
+                                    />
+                                    <TextField
+                                        id="standard"
+                                        label="Subject"
+                                        placeholder="Enter the email subject"
+                                        value={this.state.subject}
+                                        onChange={this.handleInputChangeFor('subject')}
+                                        className={classes.textField}
+                                        margin="normal"
+                                        variant="outlined"
+                                        style={{width: '64%'}}
+                                    />
+                                    <TextField
+                                        id="filled-multiline-flexible"
+                                        label="Message"
+                                        multiline
+                                        rows="8"
+                                        rowsMax="10"
+                                        value={this.state.body}
+                                        onChange={this.handleInputChangeFor('body')}
+                                        className={classes.textField}
+                                        margin="normal"
+                                        variant="outlined"
+                                        style={{width: '64%'}}
+                                    />
+                                <div>
+                                    <Button variant="contained" className={classes.button} 
+                                        style={{backgroundColor: '#b49759', marginTop: '.5rem', marginBottom: '.5rem', width: '64%', 
+                                        color: '#FFFFFF'}} onClick={(event) => this.handleSubmit(event)}>
+                                        Send Email
+                                    </Button>
+                                </div>
+                                </Paper>
+                            </form>
+                        </Grid>
+                        <Grid item xs>
+                            {this.props.addressPublicReducer.map( (item) => {
+                                return(
+                                    <Paper key= {item.id} className={classes.paper}>
+                                        <h3>Address</h3>
+                                        <p>{item.street}</p>
+                                        <p>{item.city}, {item.state} {item.zip}</p>
 
-                                <p>Tel:{item.phone}</p>
-                                <p>Fax:{item.fax}</p>
-                            </div>
-                        )
-                    })}
+                                        <p>Tel:{item.phone}</p>
+                                        <p>Fax:{item.fax}</p>
+                                    </Paper>
+                                )
+                            })}
+                            {this.props.contactPublicReducer.map( (item) => {
+                                return(
+                                    <Paper key= {item.id} className={classes.paper}>
+                                        <h3>Contact</h3>
+                                        <h4>For any additional information, please contact:</h4>
+                                        <p>{item.name}</p>
+                                        <p>{item.email}</p>
+                                    </Paper>
+                                )
+                            })}
+                        </Grid>
+                    </Grid>
                 </div>
-                <div class='contact'>
-                    {this.props.contactPublicReducer.map( (item) => {
-                        return(
-                            <div key= {item.id}>
-                                <h3>Contact</h3>
-                                <h4>For any additional information, please contact:</h4>
-                                <p>{item.name}</p>
-                                <p>{item.email}</p>
-                            </div>
-                        )
-                    })}
-                </div>
-            </div>
-            <form onSubmit={(event) => this.handleSubmit(event)}>
-                <div>
-                    <label>
-                        Your Email:
-                        <input
-                        value={this.state.email}
-                        onChange={this.handleInputChangeFor('email')}
-                        />
-                    </label>
-                </div>
-                <div>
-                    <label>
-                        Your Name:
-                        <input
-                        value={this.state.name}
-                        onChange={this.handleInputChangeFor('name')}
-                        />
-                    </label>
-                </div>
-                <div>
-                    <label>
-                        Subject:
-                        <input
-                        value={this.state.subject}
-                        onChange={this.handleInputChangeFor('subject')}
-                        />
-                    </label>
-                </div>
-                <div>
-                    <div>
-                        <label>Body:</label>
-                    </div>
-                    <div>
-                        <textarea
-                        rows="10"
-                        cols="50"
-                        value={this.state.body}
-                        onChange={this.handleInputChangeFor('body')}
-                        />
-                    </div>
-                </div>
-                <div>
-                    <button>Send Email</button>
-                </div>
-            </form>
             </Fragment>
         )
     }
@@ -128,6 +171,10 @@ const putReduxStateOnProps = (reduxStore) => ({
     addressPublicReducer: reduxStore.addressPublicReducer,
     contactPublicReducer: reduxStore.contactPublicReducer
 
-  });
+});
 
-export default connect(putReduxStateOnProps) (ContactPage);
+ContactPage.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default connect(putReduxStateOnProps)(withStyles(styles)(ContactPage));
