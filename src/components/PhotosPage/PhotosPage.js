da854e5ee9c56007be95adc028e88dd9ab167fab
Chad Dornfeld <cmdornfeld@gmail.com>
2020-02-13 10:24:20 -0600

    updated styling on PhotosPage.js

--- src/components/PhotosPage/PhotosPage.js
+++ src/components/PhotosPage/PhotosPage.js
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {withStyles} from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-
 import Nav from '../Nav/Nav';
 
+// Material UI
+import {withStyles} from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
 const styles = {
-    card: {
-        width: 'auto',
-        height: 'auto',
-        padding: 90,
-        margin: 100,
-        textAlign: 'center'
-    },
     topMargin: {
         marginTop: '8%'
+    },
+    container: {
+        width: '90%',
+        margin: '0 auto',
+        justifyContent: 'center',
+        marginTop: '2rem'
     }
 };
 
@@ -30,25 +29,23 @@ class PhotosPage extends Component {
     }
 
     render() {
+
         const {classes} = this.props;
+
         return (
             <div>
                 <Nav />
                 <div className={classes.topMargin}>
                     <h1>EVENT PHOTOS</h1>
-                        <Card className={classes.card}>
-                            <Grid container spacing={3} justify='center'>
+                            <Grid container spacing={4} className={classes.container}>
                             {this.props.photosReducer.map( (item) => {
                             return(
-                                <Grid item>
-                                <div style={{ textAlign:'center'}}>
-                                    <img src={item.url} alt={item.description} width='240px' height='200px' textAlign='center'/>
-                                </div>
+                                <Grid item key={item.id}>
+                                    <img src={item.url} alt={item.description} width='240px'/>
                                 </Grid>
                                 )
                                 })}
                             </Grid>
-                        </Card>
                 </div>
             </div>
         )
