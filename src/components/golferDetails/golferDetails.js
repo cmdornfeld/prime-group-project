2d0c11fbb35888665c297fbf4ebd7df8acfaae3f
Chad Dornfeld <cmdornfeld@gmail.com>
2020-02-13 10:25:15 -0600

    updated styling on golferDetails.js

--- src/components/golferDetails/golferDetails.js
+++ src/components/golferDetails/golferDetails.js
@@ -7,10 +7,11 @@ import { withStyles } from '@material-ui/core/styles';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
+import Button from '@material-ui/core/Button';
 
 const styles = theme => ({
     progress: {
-      margin: theme.spacing.unit * 2,
+      margin: theme.spacing(2)
     },
     linearColorPrimary: {
         backgroundColor: '#d3dee0',
@@ -19,18 +20,35 @@ const styles = theme => ({
         backgroundColor: '#b49759',
     },
     card: {
-        marginTop: 150,
-        width: 1000,
-        height: 'auto',
+        marginTop: '10%',
+        width: '60%',
         margin: '0 auto',
-        padding: 40,
-        textAlign: 'center',
-        backgroundColor: '#FFFAFA',
+        padding: '2rem',
+        backgroundColor: '#ffffff',
         color: '#253055',
-        display:'inline-block',
     },
-    topMargin: {
-        marginTop: '100px'
+    leftCard: {
+        textAlign: 'center',
+        marginTop: '2rem',
+        padding: '1.5rem'
+    },
+    pledgeButton: {
+        backgroundColor: '#b49759',
+        margin: '1rem',
+        color: '#ffffff',
+        hover: 'pointer',
+            '&:hover': {
+                backgroundColor: '#b49759'
+            }
+    },
+    backButton: {
+        backgroundColor: '#253155',
+        margin: '1rem',
+        color: '#ffffff',
+        hover: 'pointer',
+            '&:hover': {
+                backgroundColor: '#253155'
+            }
     }
 });
 
@@ -57,64 +75,36 @@ class golferDetails extends Component {
         return (
             <div>
                 <Nav />
-                <Grid container spacing={3} justify='center'>
-                    <Grid item >
-                        <Card className={classes.card}>
-                        <Grid container spacing={3} justify='center'>
-                            <Grid item sm={5}>
-                                <img src={this.props.golferIdReducer.img_url} alt={this.props.golferIdReducer.id} width='250px' />
+                <Card className={classes.card}>
+                    <Grid container spacing={2}>
+                        <Grid item xs>
+                            <div className={classes.leftCard}>
+                                <img src={this.props.golferIdReducer.img_url} alt={this.props.golferIdReducer.id} width='280px' />
                                 <h3 >Total Donations: ${parseInt(this.props.golferIdReducer.total).toLocaleString()}</h3>
                                 <LinearProgress value={normalise(golferDonationTotal)} classes={{colorPrimary: classes.linearColorPrimary, barColorPrimary: classes.linearBarColorPrimary}} 
-                                    onClick={this.handleTooltipOpen} color="primary" variant="determinate" style={{height:"1.6rem", width:"45%", borderRadius:"2rem", marginLeft:'110px'}}/>
+                                    onClick={this.handleTooltipOpen} color="primary" variant="determinate" style={{height:"1.8rem", width:"65%", borderRadius:"2rem", marginLeft:"70px"}}/>
                                 <h3>Goal: ${parseInt(this.props.golferIdReducer.goal).toLocaleString()}</h3>
-                    
-                            </Grid>
-                                {/* <div>
-                                    <h3 style={{textAlign:'center', marginTop:'20px', marginBottom:'auto' }}>{this.props.golferIdReducer.first_name} {this.props.golferIdReducer.last_name}</h3>
-                                </div>
-
-                                <div style={{textAlign:'center', width:'700px', bottom:'100px', right:'400px', marginRight:'900px', display:'inline-block'}}>
-                                    <img src={this.props.golferIdReducer.img_url} alt={this.props.golferIdReducer.id} width='220px' height='200px' />
-                                    <h3 >Total Donations: {this.props.golferIdReducer.total}</h3>
-                                    
-                                    <h3>Goal: {this.props.golferIdReducer.goal}</h3>
-                                </div>
-                                <LinearProgress value={normalise(golferDonationTotal)} classes={{colorPrimary: classes.linearColorPrimary, barColorPrimary: classes.linearBarColorPrimary}} 
-                                            onClick={this.handleTooltipOpen} color="primary" variant="determinate" style={{height:"20px", width:"20%", borderRadius:"10px", marginLeft:'200px'}}/>
-
-                                <div style={{textAlign:'center', width:'700px', bottom:'100px', right:'400px', marginLeft:'450px', marginBottom:'100px', display:'inline-block'}}>
-                                    <h3>Bio</h3>
-                                    <p>{this.props.golferIdReducer.bio}</p>
-                                    <h3>Why am I doing this?</h3>
-                                    <p >{this.props.golferIdReducer.purpose}</p>
-                                </div> */}
-                            <Grid item sm={7}>
-                                <Grid container spacing={3} justify='left'>
-                                    <Grid item sm={12}>
-                                        <div style={{float: 'left'}}>
-                                            <h2>{this.props.golferIdReducer.first_name} {this.props.golferIdReducer.last_name}</h2>
-                                        </div>
-                                    </Grid>
-                                    <Grid item sm={12}> 
-                                        <div style={{float: 'left'}}>
-                                            <h3 style={{marginLeft: '0'}}>Bio</h3>
-                                        </div>
-                                        <div style={{float: 'left', width:'600px'}}> 
-                                            <p style={{textAlign: 'left'}}>{this.props.golferIdReducer.bio}</p>
-                                        </div>
-                                        <div style={{float: 'left'}}>
-                                            <h3 style={{marginLeft: '0'}}>Why am I doing this?</h3>
-                                        </div>
-                                        <div style={{float: 'left', width:'550px'}}>
-                                            <p style={{textAlign: 'left'}}>{this.props.golferIdReducer.purpose}</p>
-                                        </div>
-                                    </Grid>
-                                </Grid>
-                            </Grid>
+                                <Button variant="contained" className={classes.pledgeButton} onClick={()=> this.props.history.push('/pledge')}>
+                                    Pledge
+                                </Button>
+                            </div>
                         </Grid>
-                        </Card>
+                        <Grid item xs>
+                            <div>
+                                <h2>{this.props.golferIdReducer.first_name} {this.props.golferIdReducer.last_name}</h2>
+                                <h3>Bio</h3>
+                                <p>{this.props.golferIdReducer.bio}</p>
+                                <h3>Why am I doing this?</h3>
+                                <p>{this.props.golferIdReducer.purpose}</p>
+                            </div>
                         </Grid>
-                </Grid>
+                    </Grid>
+                </Card>
+                <div style={{textAlign: 'center'}}>
+                    <Button variant="contained" className={classes.backButton} onClick={()=> this.props.history.push('/golfers')}>
+                        Back
+                    </Button>
+                </div>
             </div>
         );
     }
