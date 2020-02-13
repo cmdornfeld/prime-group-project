81b9a7ee497c24bbaf1ed8c2dbe214d0e4bbd550
Elmi Elmi <elmielmi522@gmail.com>
2020-02-13 09:26:19 -0600

    delete console.log in goal saga

--- src/redux/sagas/goalSaga.js
+++ src/redux/sagas/goalSaga.js
@@ -4,7 +4,6 @@ import { put, takeLatest } from 'redux-saga/effects';
 function* getGolfersGoalTotal() {
     try{
         const getResponse = yield axios.get(`/api/public/golfer-goal-total`);
-        console.log(getResponse.data)
         yield put({type: 'SET_GOLFER_GOAL_TOTAL', payload: getResponse.data})
     }
     catch (error){
@@ -15,7 +14,6 @@ function* getGolfersGoalTotal() {
 function* getGolfersDonationTotal() {
     try{
         const getResponse = yield axios.get(`/api/public/golfer-donation-total`);
-        console.log(getResponse.data)
         yield put({type: 'SET_GOLFER_DONATION_TOTAL', payload: getResponse.data})
     }
     catch (error){
