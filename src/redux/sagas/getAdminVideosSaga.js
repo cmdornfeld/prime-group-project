a6440177256db7ad729fac8e4dc251055a31d6ee
Elmi Elmi <elmielmi522@gmail.com>
2020-02-13 09:21:58 -0600

    delete console.log in get admin videos saga

--- src/redux/sagas/getAdminVideosSaga.js
+++ src/redux/sagas/getAdminVideosSaga.js
@@ -4,7 +4,6 @@ import { put, takeLatest } from 'redux-saga/effects';
 function* getAdminVideos() {
     try{
         const getResponse = yield axios.get(`/api/admin/videos`);
-        console.log(getResponse.data)
         yield put({type: 'SET_VIDEOS', payload: getResponse.data})
     }
     catch (error){
